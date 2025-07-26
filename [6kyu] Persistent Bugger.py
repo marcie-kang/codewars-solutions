fdc692bb2bf5d9028934a12be2035296ecb54de4
marcie <kangsorom@gmail.com>
2025-07-26 17:34:35 +1000

    Create [6kyu] Persistent Bugger.py

---
+++ [6kyu] Persistent Bugger.py
@@ -0,0 +1,28 @@
+"""
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit, there are 3 multiplications)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2, there are 4 multiplications)
+4 --> 0 (because 4 is already a one-digit number, there is no multiplication)
+"""
+
+
+def persistence(n):
+    converted = str(n)
+    is_on = True
+    answer = 0
+
+    while is_on:
+        multiple = 1
+
+        if len(converted) == 1:
+            is_on = False
+            return answer
+
+        for num in range(len(converted)):
+            multiple *= int(converted[num])
+
+        converted = str(multiple)
+        answer += 1
