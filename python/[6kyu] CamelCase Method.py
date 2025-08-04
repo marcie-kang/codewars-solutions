c670253764c2e6ce34e7ef3260e7be1f08dd8047
marcie <kangsorom@gmail.com>
2025-08-04 21:13:09 +1000

    python_040825

---
+++ python/[6kyu] CamelCase Method.py
@@ -0,0 +1,19 @@
+"""
+Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+
+Examples (input --> output):
+"hello case" --> "HelloCase"
+"camel case word" --> "CamelCaseWord"
+
+Don't forget to rate this kata! Thanks :)
+"""
+
+def camel_case(s):
+    list = s.split()
+
+    return "".join([char.capitalize() for char in list])
+
+#second answer
+
+def camel_case(s):
+    return s.title().replace(" ", "")
