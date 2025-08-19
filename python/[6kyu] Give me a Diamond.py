1c40ee614fa2a2e9327fadac6e7722509b10415c
marcie <kangsorom@gmail.com>
2025-08-19 17:41:39 +1000

    python_190825

---
+++ python/[6kyu] Give me a Diamond.py
@@ -0,0 +1,49 @@
+"""
+Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+Task
+You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+Examples
+A size 3 diamond:
+
+ *
+***
+ *
+...which would appear as a string of " *\n***\n *\n"
+
+A size 5 diamond:
+
+  *
+ ***
+*****
+ ***
+  *
+...that is:
+
+"  *\n ***\n*****\n ***\n  *\n"
+"""
+
+def diamond(n):
+    if n % 2 == 0 or n <= 0:
+        return None
+
+    diamond = "*"
+    answer = ""
+    space = " " * (n // 2)
+    count = (n // 2) + 1
+
+    while count > 0:
+        answer += space + diamond + "\n"
+        diamond += "**"
+        space = space[:-1]
+        count -= 1
+
+    diamond_list = answer.split("\n")
+
+    for i in range(len(diamond_list) - 3, -1, -1):
+        answer += diamond_list[i] + "\n"
+
+    return answer
