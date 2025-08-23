2a894b523ac9841b70fca7c7d47e8eb86039a802
marcie <kangsorom@gmail.com>
2025-08-23 17:04:21 +1000

    python_220825

---
+++ python/[6kyu] Encrypt this!.py
@@ -0,0 +1,39 @@
+"""
+Acknowledgments:
+I thank yvonne-liu for the idea and for the example tests :)
+
+Description:
+Encrypt this!
+
+You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+Your message is a string containing space separated words.
+You need to encrypt each word in the message using the following rules:
+The first letter must be converted to its ASCII code.
+The second letter must be switched with the last letter
+Keepin' it simple: There are no special characters in the input.
+Examples:
+encrypt_this("Hello") == "72olle"
+encrypt_this("good") == "103doo"
+encrypt_this("hello world") == "104olle 119drlo"
+"""
+
+def encrypt_this(text):
+    if text == "":
+        return ""
+
+    answer = []
+
+    for word in text.split(" "):
+        converted = ""
+
+        if len(word) == 1:
+            converted = str(ord(word))
+        elif len(word) == 2:
+            converted = str(ord(word[0])) + word[1]
+        else:
+            converted = str(ord(word[0])) + word[-1] + word[2:-1] + word[1]
+
+        answer.append(converted)
+
+    return " ".join(answer)
