b26cb6f5edca19f66973218be85ea5801d66a28f
marcie <kangsorom@gmail.com>
2025-09-09 10:09:01 +1000

    python_090925

---
+++ python/[5kyu] Moving Zeros To The End.py
@@ -0,0 +1,11 @@
+"""
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+"""
+
+def move_zeros(lst):
+    removed = [x for x in lst if x != 0]
+    removed += ([0] * (len(lst) - len(removed)))
+
+    return removed
