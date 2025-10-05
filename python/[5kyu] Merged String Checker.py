38da4bb02372c0be6492e22af4c4a2e46974f2ee
marcie <kangsorom@gmail.com>
2025-10-05 15:12:01 +1000

    python_051025

---
+++ python/[5kyu] Merged String Checker.py
@@ -0,0 +1,31 @@
+"""
+At a job interview, you are challenged to write an algorithm to check if a given string, s, can be formed from two other strings, part1 and part2.
+
+The restriction is that the characters in part1 and part2 should be in the same order as in s.
+
+The interviewer gives you the following example and tells you to figure out the rest from the given test cases.
+
+For example:
+
+'codewars' is a merge from 'cdw' and 'oears':
+
+    s:  c o d e w a r s   = codewars
+    part1:  c   d   w         = cdw
+    part2:    o   e   a r s   = oears
+"""
+
+def is_merge(s, part1, part2):
+    if not s:
+        return not part1 and not part2
+
+    can_merge = False
+
+    if part1 and s[0] == part1[0]:
+        if is_merge(s[1:], part1[1:], part2):
+            can_merge = True
+
+    if not can_merge and part2 and s[0] == part2[0]:
+        if is_merge(s[1:], part1, part2[1:]):
+            can_merge = True
+
+    return can_merge
