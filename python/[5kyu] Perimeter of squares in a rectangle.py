40fdaae888718a1e989e838f00ed8dd9b4785443
marcie <kangsorom@gmail.com>
2026-03-11 20:38:56 +1000

    python_110326

---
+++ python/[5kyu] Perimeter of squares in a rectangle.py
@@ -0,0 +1,27 @@
+"""
+The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80
+Could you give the sum of the perimeters of all the squares in a rectangle when there are n + 1 squares disposed in the same manner as in the drawing:
+
+alternative text
+
+Hint:
+See Fibonacci sequence
+
+Ref:
+http://oeis.org/A000045
+
+The function perimeter has for parameter n where n + 1 is the number of squares (they are numbered from 0 to n) and returns the total perimeter of all the squares.
+
+perimeter(5)  should return 80
+perimeter(7)  should return 216
+"""
+
+def perimeter(n):
+    a, b = 1, 1
+    total_sum = 0
+
+    for _ in range(n + 1):
+        total_sum += a
+        a, b = b, a + b
+
+    return total_sum * 4
